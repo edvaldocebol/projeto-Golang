1b1baf79f022f4465088a9fce0432a9a46b7fab1
Edson junior <ej91720@gmail.com>
2023-12-12 19:53:04 -0300

    feat: add routers logic

--- router/router.go
+++ router/router.go
@@ -3,11 +3,7 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Inisialize() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run(":8080")
+	router := gin.Default()
+	InitializeRouter(router)
+	router.Run(":8080")
 }
