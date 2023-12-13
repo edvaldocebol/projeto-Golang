856595fbe5d38501c06b0757a49aa024a2c9b9a7
Edson junior <ej91720@gmail.com>
2023-12-12 22:07:29 -0300

    feat: add opening schema

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/edvaldocebol/projeto-Golang/router"
+import (
+	"github.com/edvaldocebol/projeto-Golang/config"
+	"github.com/edvaldocebol/projeto-Golang/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
-	router.Inisialize()
+	logger = config.GetLogger("main")
+	err := config.Init()
+	if err != nil { 
+		logger.Errorf("config initialization error %v", err)
+		return
+	}
+	router.Inisialize() 
 }
